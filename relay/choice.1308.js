d4f5df086ef31734ee1d33e4db69962caff86f4d
JamesDowney <james.m.downey30@gmail.com>
2023-01-04 15:46:47 -0700

    testing in monorail

--- relay/choice.1308.js
+++ relay/choice.1308.js
@@ -1,12 +1,17 @@
-const kol = require("kolmafia");
-const { doubleConfirm } = require("./choice-confirm/choice-confirm");
+const kol = require('kolmafia')
+const { addConfirmation } = require('./choice-confirm/choice-confirm')
+const { choiceOverrideDecodePageText } = require('relay/choice.ash')
 
 // https://kol.coldfront.net/thekolwiki/index.php/On_a_Downtown_Train
 
-module.exports.main = function (page_text_encoded)
-{
-	var choice_override_script = require("relay/choice.ash");
-	var page_text = choice_override_script.choiceOverrideDecodePageText(page_text_encoded);
-	page_text = page_text.replace(`value="Just ride the rails (9 Adventures)">`, `onclick="return doubleConfirm()" value="Just ride the rails (9 Adventures)"> ${doubleConfirm}`);
-	kol.write(page_text);
+module.exports.main = function (page_text_encoded) {
+    const page_text =
+        choiceOverrideDecodePageText(page_text_encoded)
+
+    var badChoices = [
+        1,
+        2,
+    ]
+
+    kol.write(addConfirmation(page_text, badChoices))
 }

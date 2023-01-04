11b4232d3942d3e35418cd76cde246ae339c8c0d
JamesDowney <james.m.downey30@gmail.com>
2023-01-04 15:31:56 -0700

    here we go again

--- relay/choice.111.js
+++ relay/choice.111.js
@@ -1,50 +1,14 @@
 const kol = require('kolmafia')
 const { addConfirmation } = require('./choice-confirm/choice-confirm')
-const choice_override_script = require('relay/choice.ash')
-
-const choice = {
-    "111": {
-        "Serve your sentence": 1,
-        "Rise and revolt": 2,
-        "Plot a cunning escape": 3
-    },
-    "113": {},
-    "153": {},
-    "155": {},
-    "157": {},
-    "178": {},
-    "182": {},
-    "503": {},
-    "505": {},
-    "506": {},
-    "523": {},
-    "611": {},
-    "669": {},
-    "670": {},
-    "671": {},
-    "672": {},
-    "673": {},
-    "674": {},
-    "675": {},
-    "676": {},
-    "677": {},
-    "678": {},
-    "923": {},
-    "924": {},
-    "925": {},
-    "926": {},
-    "927": {},
-    "928": {},
-    "1026": {},
-    "1308": {}
-}
+const { choiceOverrideDecodePageText } = require('relay/choice.ash')
+const choice = require('./choice-confirm/choice_map.json')
 
 
 // https://kol.coldfront.net/thekolwiki/index.php/Malice_in_Chains
 
 module.exports.main = function (page_text_encoded) {
     const page_text =
-        choice_override_script.choiceOverrideDecodePageText(page_text_encoded)
+        choiceOverrideDecodePageText(page_text_encoded)
 
     var badChoices = [
         choice[111]['Rise and revolt'],

f02bc8a949d5a6ec929867e827e3c23cc498b2b6
JamesDowney <james.m.downey30@gmail.com>
2023-01-04 16:08:06 -0700

    try again with json

--- relay/choice.111.js
+++ relay/choice.111.js
@@ -1,43 +1,7 @@
 const kol = require('kolmafia')
 const { addConfirmation } = require('./choice-confirm/choice-confirm')
 const { choiceOverrideDecodePageText } = require('relay/choice.ash')
-const choice = {
-    "111": {
-        "Serve your sentence": 1,
-        "Rise and revolt": 2,
-        "Plot a cunning escape": 3
-    },
-    "113": {},
-    "153": {},
-    "155": {},
-    "157": {},
-    "178": {},
-    "182": {},
-    "503": {},
-    "505": {},
-    "506": {},
-    "523": {},
-    "611": {},
-    "669": {},
-    "670": {},
-    "671": {},
-    "672": {},
-    "673": {},
-    "674": {},
-    "675": {},
-    "676": {},
-    "677": {},
-    "678": {},
-    "923": {},
-    "924": {},
-    "925": {},
-    "926": {},
-    "927": {},
-    "928": {},
-    "1026": {},
-    "1308": {}
-}
-
+const choice = require('./choice-confirm/choice_map.json')
 
 // https://kol.coldfront.net/thekolwiki/index.php/Malice_in_Chains
 
